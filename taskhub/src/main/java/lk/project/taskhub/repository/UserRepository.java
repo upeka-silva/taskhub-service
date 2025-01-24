1fa799460b08948f7e2f27d3484e6141e0d034f8
upeka-silva <upekasilva2131@gmail.com>
2025-01-24 22:36:18 +0530

    user service implemented

---
+++ taskhub/src/main/java/lk/project/taskhub/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package lk.project.taskhub.repository;
+
+import lk.project.taskhub.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u Where u.email =:email")
+    Optional<User> findByEmail(@Param("email") String email);
+
+}
