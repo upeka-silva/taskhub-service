ceaa69185ca3dcd28c7e715179b7a5876ac38fe5
upeka-silva <upekasilva2131@gmail.com>
2025-01-26 19:06:08 +0530

    changes

---
+++ taskhub/src/main/java/lk/project/taskhub/Exceptions/UserNotVerifiedException.java
@@ -0,0 +1,7 @@
+package lk.project.taskhub.Exceptions;
+
+public class UserNotVerifiedException extends RuntimeException {
+    public UserNotVerifiedException(String message) {
+        super(message);
+    }
+}
